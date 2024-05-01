280ea135d7e7279d2f6476c200c294c4dc6b7fa7
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-01 10:15:46 +0000

    render component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  return !loading ? (
+    <div className="min-h-screen w-full flex flex-wrap items-center justify-center">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
