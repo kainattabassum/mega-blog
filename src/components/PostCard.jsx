56be000e257283565a4adc47296d2975c7c4568e
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-08 05:54:56 +0000

    update post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import dbService from "../appwrite/config";
 
-function PostCard() {
-  return <div className=""></div>;
+function PostCard({ $id, title, featureImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full flex justify-center mn-4">
+          <img
+            src={dbService.getFilePreview(featureImage)}
+            alt={title}
+            className="rounded-xl"
+          />
+        </div>
+        <h4 className="text-xl font-bold">{title}</h4>
+      </div>
+    </Link>
+  );
 }
 
 export default PostCard;
