5bfb05685a8f22f3273e6ff8cb642a443e4631c9
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-20 06:34:09 +0000

    update editpost compo

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, PostForm } from "../components";
+import appwriteService from "../appwrite/config";
+import { useParams, useNavigate } from "react-router-dom";
 
 function EditPost() {
-  return (
-    <div>EditPost</div>
-  )
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+  return post ? (
+    <div>
+      <Container>
+        <PostForm post={post} />
+      </Container>
+    </div>
+  ) : null;
 }
 
-export default EditPost
+export default EditPost;
