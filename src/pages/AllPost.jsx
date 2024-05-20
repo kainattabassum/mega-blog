e21cd4335ca791e892f32734c9099ccbc54c55d9
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-20 06:29:19 +0000

    update allpost component

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/config";
+import { Container, PostCard } from "../components";
 
 function AllPost() {
+  const [posts, setPost] = useState({});
+
+  useEffect(() => {}, []);
+  appwriteService.getPosts([]).then((posts) => {
+    if (posts) {
+      setPosts(posts.documents);
+    }
+  });
   return (
-    <div className="w-full py-8">AllPost</div>
-  )
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default AllPost
+export default AllPost;
