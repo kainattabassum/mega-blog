7bae96619e595614c433083b3fd40250a866c144
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-21 05:41:45 +0000

    update post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import { Button, Container } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
 
 export default function Post() {
   return (
