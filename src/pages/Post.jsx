a6aad7074e2d3cc8d591a04e9a654dea2a9aa6b9
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-21 05:42:08 +0000

    update post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -6,7 +6,30 @@ import parse from "html-react-parser";
 import { useSelector } from "react-redux";
 
 export default function Post() {
-  return (
-    <div>Post</div>
-  )
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) setPost(post);
+        else navigate("/");
+      });
+    } else navigate("/");
+  }, [slug, navigate]);
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+  return <div>Post</div>;
 }
