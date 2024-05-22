fce34d8da10888f6c1076099ea537e0566cac7ce
Kainat Tabassum <53490506+kainattabassum@users.noreply.github.com>
2024-05-22 05:36:24 +0000

    rdebug

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import appwriteService from "../appwrite/config";
 import { Button, Container } from "../components";
-import parse from "html-react-parser";
 import { useSelector } from "react-redux";
+import HtmlParser from "react-html-parser";
 
 export default function Post() {
   const [post, setPost] = useState(null);
@@ -57,7 +57,7 @@ export default function Post() {
         <div className="w-full mb-6">
           <h1 className="text-2xl font-bold">{post.title}</h1>
         </div>
-        <div className="browser-css">{parse(post.content)}</div>
+        <div className="browser-css">{HtmlParser(post.content)}</div>
       </Container>
     </div>
   ) : null;
